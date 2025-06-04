350484083f832fa7311b5f80b92d57a56566bfdc
ryanshihi <ryanshi335@gmail.com>
2025-06-04 21:25:03 +1000

    Create template.py

---
+++ template.py
@@ -0,0 +1,34 @@
+def task1(size):
+    print("\nBegin Task 1 with size", size)
+    # Add your code here
+
+def task2a(size):
+    print("\nBegin Task 2A with size", size)
+    # Add your code here
+
+def task2b(size):
+    print("\nBegin Task 2B with size", size)
+    # Add your code here
+
+def task3(size):
+    print("\nBegin Task 3 with size", size)
+    # Add your code here
+
+def task4(size):
+    print("\nBegin Task 4 with size", size)
+    # Add your code here
+
+def task5(size):
+    print("\nBegin Task 4 with size", size)
+    # Add your code here
+    
+if __name__ == "__main__":
+    size = input("Enter size: ")
+    
+    task1(size)
+    task2a(size)
+    task2b(size)
+    task3(size)
+    task4(size)
+    task4(size)
+    input()
